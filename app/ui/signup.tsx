fa319fa2e938dd2b9fd1b846b3687f1ea7085757
rrecord <137208283+rrecord@users.noreply.github.com>
2024-09-12 22:46:49 +0400

    df

--- app/ui/signup.tsx
+++ app/ui/signup.tsx
@@ -137,16 +137,16 @@ export default function Register() {
       </h1>
       <form onSubmit={handleSubmit(onSubmit)} className="w-full mt-3">
         <div className="grid grid-cols-2">
-          <div className="mb-2 rounded-tl-md rounded-bl-md">
+          <div className="mb-2">
             <input
               type="number"
-              className="px-2 py-2 w-full rounded-tl-md rounded-bl-md outline-none dark:focus:bg-white/[0.01] dark:bg-white/[0.03] focus:bg-black/[0.12] bg-black/[0.08] backdrop-blur-xl transition-colors placeholder:text-sm placeholder:font-semibold dark:placeholder:text-white/50 placeholder:text-black/60 font-semibold"
+              className="px-2 py-2 w-full outline-none dark:focus:bg-white/[0.01] dark:bg-white/[0.03] focus:bg-black/[0.12] bg-black/[0.08] backdrop-blur-xl transition-colors placeholder:text-sm placeholder:font-semibold dark:placeholder:text-white/50 placeholder:text-black/60 font-semibold"
               autoComplete="off"
               placeholder="User ID"
               {...register("userId", { required: "User ID is required" })}
             />
           </div>
-          <div className="mb-2 flex items-center justify-between w-full px-3 rounded-tr-md rounded-br-md dark:bg-white/[0.03] bg-black/[0.08] py-2.5">
+          <div className="mb-2 -ml-1 flex items-center justify-between w-full px-3 rounded-tr-md rounded-br-md dark:bg-white/[0.03] bg-black/[0.08] py-2.5">
             <span className="text-sm font-semibold block dark:text-white/50 text-black/60">
               Role
             </span>
